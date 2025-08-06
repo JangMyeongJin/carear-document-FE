bc206dd7aaa8245a719eeddb587db6c5b98f8b32
JangMyeongJin <jean9710@naver.com>
2025-08-06 15:17:05 +0900

    skills, Education 수정

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -18,14 +18,11 @@ const Hero = () => {
               안녕하세요. <br />
               저는 <span className="highlight">장명진</span>입니다
             </h1>
-            <p className="hero-subtitle">
-              주로 검색엔진 운영과 검색 API 개발을 통해 사용자에게 정확하고
-              빠른 검색 경험을 제공해왔습니다. <br />
-              사용자가 쉽게 검색 할 수 있게 인기 검색어, 자동완성, 데이터 통계
-              API 등 부가적인 API도 개발해왔습니다. <br />
-              사용자 의도를 정확히 파악하기 위해 OpenSearch를 이용하여Vector
-              검색과LLM을 연동한 AI 검색 서비스 개발 경험도 있습니다.
-            </p>
+            <ul className="hero-subtitle">
+              <li>주로 검색엔진 운영과 검색 API 개발을 통해 사용자에게 정확하고 빠른 검색 경험을 제공해왔습니다.</li>
+              <li>사용자가 쉽게 검색 할 수 있게 인기 검색어, 자동완성, 데이터 통계 API 등 부가적인 API도 개발해왔습니다.</li>
+              <li>사용자 의도를 정확히 파악하기 위해 OpenSearch를 이용하여 Vector 검색과 LLM을 연동한 AI 검색 서비스 개발 경험도 있습니다.</li>
+            </ul>
             <div className="hero-buttons">
               <button
                 className="btn btn-primary"
